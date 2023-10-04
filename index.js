50a6060a78104a9093de486ad56410adb05e05b1
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-04 11:01:49 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM content loaded");
+    let paraText = document.querySelector('#text');
+    paraText.textContent = "This is really cool!"
+})
+
+console.log("Before DOM content loaded")
